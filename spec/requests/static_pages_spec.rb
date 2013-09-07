42992c5f06284106fe2c97aa4c81547c25a7ad0b
Emma Lubin <elubin@mit.edu>
2013-09-07 17:22:13 -0400

    added titles to static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,9 +8,40 @@ describe "StaticPages" do
   #    response.status.should be(200)
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the content 'Scheduler App'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_content('Scheduler App')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Scheduler App | Home")
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Scheduler App | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Scheduler App | About Us")
     end
   end
 end
